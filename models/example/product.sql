db20a81c4dbc8753e3eea11484d27d79c4658f45
ayyappa reddy vempalli <reddyvempalli300@gmail.com>
2024-04-24 06:32:09 +0000

    init

---
+++ models/example/product.sql
@@ -0,0 +1,10 @@
+-- File: products.sql
+-- Description: Define model for products table
+
+SELECT
+    product_id,
+    product_name,
+    category,
+    price
+FROM
+    {{ source('dbt_avempalli', 'products') }};
